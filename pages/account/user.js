04feb99df272eacf4b6ee052ed942f53b1339ac8
Janusz Wozniak <januszwozniak.bth@gmail.com>
2022-10-06 15:03:47 +0100

    something

--- pages/account/user.js
+++ pages/account/user.js
@@ -1,17 +1,27 @@
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react"
-import styles from "./user.module.css"
+import Link from "next/link";
 
 
 function userPage() {
     const {data : session} = useSession()
-    console.log(session)
-    return (
-        <div className={styles.container}>
+    {
+        if (!session) {
+            return (
+                <div className="pl-8">
+                    Please <Link href="/login"><a>Login</a></Link>
+                </div>
+            )
+        } else {
+            return (
+        <div className='pl-8'>
             <p>Hello {session.user.name}</p>
             <button onClick={() => signOut({callbackUrl: "/"})}>Sign Out</button>
         </div>
     )
+        }
+    }
+    
 }
 
 export default userPage;
